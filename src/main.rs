ea45508c2bacb7fa0a266c9cb8d2044dee02eb45
荀洪道 <kalxd@126.com>
2024-12-11 19:50:47 +0800

    gtk也是一堆坑。

--- src/main.rs
+++ src/main.rs
@@ -9,28 +9,29 @@ mod widget;
 mod window;
 
 fn setup_ui(app: &Application) {
+	let main_layout = GtkBox::builder()
+		.orientation(Orientation::Vertical)
+		.spacing(10)
+		.margin(20)
+		.build();
+
 	let amount_spin_btn = SpinButton::with_range(0_f64, 100_f64, 1_f64);
 	amount_spin_btn.set_value(10_f64);
 
 	let form = widget::Form::new("设置");
+
+	main_layout.pack_start(&form.container, false, false, 10);
+
 	form.add_row("题目数量", &amount_spin_btn);
 
 	let submit_btn = Button::with_label("开始做题");
+	main_layout.pack_end(&submit_btn, true, false, 0);
 	submit_btn.connect_clicked(clone!(@weak app => move |_| {
 		let sub_window = window::SubWindow::new();
 		app.add_window(&sub_window.window);
 
 		sub_window.show();
 	}));
-
-	let main_layout = GtkBox::builder()
-		.orientation(Orientation::Vertical)
-		.spacing(10)
-		.margin(20)
-		.build();
-	main_layout.pack_start(&amount_spin_btn, false, false, 10);
-	main_layout.pack_start(&form.container, false, false, 10);
-	main_layout.pack_end(&submit_btn, true, false, 0);
 
 	let window = ApplicationWindow::builder()
 		.application(app)
