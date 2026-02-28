3599c4a9bdc5c3be19493eb61efedd2bc9f34359
荀洪道 <kalxd@126.com>
2026-02-28 23:00:19 +0800

    投骰子。

--- src/app/touzi/touzi.ts
+++ src/app/touzi/touzi.ts
@@ -1,9 +1,43 @@
 import { Component } from '@angular/core';
+import { ActionResult, UiTaskDirective } from "drifloon";
+import * as R from "rxjs";
+
+const rand = (): number => {
+	return Math.floor(Math.random() * 6) + 1;
+};
+
+interface TouziResult {
+	a: number;
+	b: number;
+}
 
 @Component({
   selector: 'app-touzi',
-  imports: [],
+  imports: [UiTaskDirective],
   templateUrl: './touzi.html',
   styleUrl: './touzi.css',
 })
-export class Touzi {}
+export class Touzi {
+	protected result$: R.Observable<ActionResult<TouziResult>>;
+
+	private start$ = new R.Subject<void>();
+
+	constructor() {
+		this.result$ = this.start$
+			.pipe(
+				ActionResult.exhaustMap(_ => {
+					const a = rand();
+					const b = rand();
+					return R.timer(1000).pipe(R.map(_ => ({ a, b })));
+				}),
+				R.startWith(ActionResult.Ok({
+					a: 0,
+					b: 0
+				}))
+			);
+	}
+
+	startTouzi(): void {
+		this.start$.next();
+	}
+}
