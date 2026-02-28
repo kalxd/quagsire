daa0377644e26d9bd2aa25475752d855619c2caf
荀洪道 <kalxd@126.com>
2026-02-28 22:26:20 +0800

    几个页面。

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from "./home/home";
+import { Touzi } from "./touzi/touzi";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: "",
+		component: Home
+	},
+	{
+		path: "touzi",
+		component: Touzi
+	}
+];
---
+++ src/app/touzi/touzi.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-touzi',
+  imports: [],
+  templateUrl: './touzi.html',
+  styleUrl: './touzi.css',
+})
+export class Touzi {}
